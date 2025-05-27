c7dfc16f9253623337ac74b1acfdf2c97bb2207c
muhammadfahaddev <fahad.programmer34@gmail.com>
2025-05-27 18:50:07 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
+// vite.config.js
+
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     react(),
     tailwindcss(),
+
   ],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/firebase')) {
+            return 'firebase';
+          }
+        }
+      }
+    }
+  }
+});
